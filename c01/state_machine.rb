03894fb48b3e9090c0733e5e30a1f030a560d58a
soasme <soasme@gmail.com>
2013-07-25 08:33:39 +0800

    `add_actions` and `add_transitions` for State.

--- c01/state_machine.rb
+++ c01/state_machine.rb
@@ -10,6 +10,26 @@ class AbstractEvent
 end
 
 class State < AbstractEvent
+  attr_accessor :name, :actions, :transitions
+
+  def initialize(name)
+    @name = name
+    @actions = []
+    @transitions = {}
+  end
+
+  def add_actions(*commands)
+    puts commands
+    commands.each { |command|
+      @actions << command
+    }
+  end
+
+  def add_transitions(dict)
+    dict.each { |key, value|
+      @transitions[key] = value
+    }
+  end
 end
 
 class Command < AbstractEvent
