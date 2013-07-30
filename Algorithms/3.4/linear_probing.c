d1b871a27e3cc9920ab15e70602cb729dda7c179
soasme <soasme@gmail.com>
2013-07-30 08:47:08 +0800

    Framework of linear probing table.

---
+++ Algorithms/3.4/linear_probing.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+typedef char* string;
+
+typedef struct {
+    string* keys;
+    int* values;
+} LinearProbing;
+
+void linear_probing_init(LinearProbing* table);
+int linear_probing_hash(LinearProbing* table, string key);
+void linear_probing_set(LinearProbing* table, string key, int value);
+int linear_probing_get(LinearProbing* table, string key);
+void linear_probing_delete(LinearProbing* table, string key);
+
+int
+main(int argv, char** args){
+    LinearProbing* table;
+    int hello;
+    linear_probing_init(table);
+
+    hello = linear_probing_get(table, "hello");
+    printf("first get: %d\n", hello);
+
+    linear_probing_set(table, "hello", 1);
+
+    hello = linear_probing_get(table, "hello");
+    printf("get after set: %d\n", hello);
+
+    linear_probing_delete(table, "hello");
+
+    hello = linear_probing_get(table, "hello");
+    printf("get after delete: %d\n", hello);
+
+    return 0;
+}
+
+void linear_probing_init(LinearProbing* table){
+}
+
+int linear_probing_hash(LinearProbing* table, string key){
+
+}
+
+void linear_probing_set(LinearProbing* table, string key, int value){
+
+}
+
+int linear_probing_get(LinearProbing* table, string key){
+
+}
+
+void linear_probing_delete(LinearProbing* table, string key){
+
+}
