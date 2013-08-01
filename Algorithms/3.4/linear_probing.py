68b07fd91cd3106dd19bbc43144a5c7d95f18d2e
soasme <soasme@gmail.com>
2013-08-01 09:39:33 +0800

    Add delete method for linear probing hash table.

--- Algorithms/3.4/linear_probing.py
+++ Algorithms/3.4/linear_probing.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+class NullItem(object):
+    def __repr__(self):
+        return 'Null'
+nullitem = NullItem()
+
 class LinearProbingHashTable(object):
 
     def get(self, key):
@@ -26,6 +31,11 @@ class LinearProbingHashTable(object):
                 self.values[index] = value
                 self.existing_keys += 1
                 return
+            if self.keys[index] and self.keys[index] == nullitem:
+                self.keys[index] = key
+                self.values[index] = value
+                self.existing_keys += 1
+                return
             if self.keys[index] and self.keys[index] == key:
                 self.values[index] = value
                 self.existing_keys += 1
@@ -35,7 +45,22 @@ class LinearProbingHashTable(object):
                 raise Exception
 
     def delete(self, key):
-        pass
+        if self.existing_keys * 8 <= self.size:
+            self.resize(self.size / 2)
+
+        start = self.hash(key)
+        index = start
+        while True:
+            if not self.keys[index]:
+                return
+            if self.keys[index] and self.keys[index] == key:
+                self.keys[index] = nullitem
+                self.existing_keys -= 1
+                return
+            index = (index + 1) % self.size
+            if index == start:
+                return
+
 
     def resize(self, size):
         new_keys, new_values = self.init(size)
@@ -82,13 +107,19 @@ class LinearProbingHashTable(object):
 
 table = LinearProbingHashTable(5)
 table.set('hello', 1)
-print table.keys, table.values
+print table.keys, table.values, table.existing_keys
 print table.get('hello')
 table.set('hello', 2)
 print table.get('hello')
-print table.keys, table.values
+print table.keys, table.values, table.existing_keys
 for i in range(4):
     key = "hello/%s" % i
     table.set(key, i)
     print table.get(key)
-    print table.keys, table.values
+    print table.keys, table.values, table.existing_keys
+
+for i in range(4):
+    key = "hello/%s" % i
+    table.delete(key)
+    print table.get(key)
+    print table.keys, table.values, table.existing_keys, table.size
